a63db949cd9afecb0c7664d9d15717a63080532f
nickolay.lebediev <nickolay.le@gmail.com>
2017-11-07 17:59:55 +0200

    Store previous prices (updates once a min)
    TODO:
    while/break
    change in %

--- info.py
+++ info.py
@@ -5,15 +5,18 @@ import time
 from functools import reduce
 
 
+# parse dict
 def get_from_dict(data_dict, map_list):
     return reduce(operator.getitem, map_list, data_dict)
 
 
+# get prices info for chosen pair
 def url(pair):
     symbol = 'https://wex.nz/api/3/ticker/' + pair
     return symbol
 
 
+# info url with list of available pairs
 pairs_url = 'https://wex.nz/api/3/info'
 
 http = httplib2.Http()
@@ -25,13 +28,29 @@ print("Server time :", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(get_fro
 
 
 user_input = input("Please enter pair symbol: ")
-try:
-    data_response, data_content = http.request(url(user_input), 'GET')
-    data = json.loads(data_content.decode('utf-8'))
+# temp dictionary
+previous_data = False
+data_content = False
+while True:
+    try:
 
-    print("High " + user_input + ": ", get_from_dict(data, [user_input, "high"]))
-    print("Low " + user_input + ": ", get_from_dict(data, [user_input, "low"]))
-    print("Last " + user_input + ":", get_from_dict(data, [user_input, "last"]))
-    print("Updated :", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(get_from_dict(data, [user_input, "updated"]))))
-except KeyError as key_err:
-    print("There's no such pair: ", key_err)
+        if previous_data:
+            data = json.loads(data_content.decode('utf-8'))
+            print("prev High " + user_input + ": ", get_from_dict(data, [user_input, "high"]))
+            print("prev Low " + user_input + ": ", get_from_dict(data, [user_input, "low"]))
+            print("prev Last " + user_input + ":", get_from_dict(data, [user_input, "last"]))
+            print("prev Updated :",
+                  time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(get_from_dict(data, [user_input, "updated"]))))
+
+        data_response, data_content = http.request(url(user_input), 'GET')
+        data = json.loads(data_content.decode('utf-8'))
+
+        print("High " + user_input + ": ", get_from_dict(data, [user_input, "high"]))
+        print("Low " + user_input + ": ", get_from_dict(data, [user_input, "low"]))
+        print("Last " + user_input + ":", get_from_dict(data, [user_input, "last"]))
+        print("Updated :",
+              time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(get_from_dict(data, [user_input, "updated"]))))
+        previous_data = data
+        time.sleep(60)
+    except KeyError as key_err:
+        print("There's no such pair: ", key_err)
