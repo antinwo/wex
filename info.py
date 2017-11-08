b9604fc3d17059ce75e22aa11b9e8654068395b2
nickolay.lebediev <nickolay.le@gmail.com>
2017-11-08 14:09:24 +0200

    absolute change change in % (in progress)
    TODO:
    while/break

--- info.py
+++ info.py
@@ -3,6 +3,7 @@ import json
 import operator
 import time
 from functools import reduce
+from decimal import Decimal
 
 
 # parse dict
@@ -26,31 +27,42 @@ pairs = json.loads(pair_content.decode('utf-8'))
 print("Available pair list: ", list(pairs["pairs"].keys()))
 print("Server time :", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(get_from_dict(pairs, ["server_time"]))))
 
-
 user_input = input("Please enter pair symbol: ")
 # temp dictionary
-previous_data = False
+previous_data = {}
+change = Decimal(0)
 data_content = False
 while True:
     try:
 
         if previous_data:
+            change = Decimal(0)
             data = json.loads(data_content.decode('utf-8'))
             print("prev High " + user_input + ": ", get_from_dict(data, [user_input, "high"]))
             print("prev Low " + user_input + ": ", get_from_dict(data, [user_input, "low"]))
             print("prev Last " + user_input + ":", get_from_dict(data, [user_input, "last"]))
-            print("prev Updated :",
+            print("prev Updated:",
                   time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(get_from_dict(data, [user_input, "updated"]))))
 
         data_response, data_content = http.request(url(user_input), 'GET')
         data = json.loads(data_content.decode('utf-8'))
 
+        # calculating price change
+        if user_input in previous_data:
+            change = Decimal(
+                get_from_dict(data, [user_input, "last"])) - Decimal(get_from_dict(previous_data, [user_input, "last"]))
+        else:
+            change = Decimal(0)
+
         print("High " + user_input + ": ", get_from_dict(data, [user_input, "high"]))
         print("Low " + user_input + ": ", get_from_dict(data, [user_input, "low"]))
         print("Last " + user_input + ":", get_from_dict(data, [user_input, "last"]))
-        print("Updated :",
+        print("Updated:",
               time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(get_from_dict(data, [user_input, "updated"]))))
+        print("Aboslute change: ", change.quantize(Decimal('0.01')))
+
         previous_data = data
-        time.sleep(60)
+
+        time.sleep(30)
     except KeyError as key_err:
         print("There's no such pair: ", key_err)
